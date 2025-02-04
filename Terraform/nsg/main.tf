ed13417b276e49bb8034d390858f83fb333d8ed4
Iván Sánchez <ivsancheza@acciona.com>
2025-02-04 11:28:03 +0100

    Se añaden tablas de enrutamiento y reglas de seguridad

--- Terraform/nsg/main.tf
+++ Terraform/nsg/main.tf
@@ -86,4 +86,28 @@ resource "azurerm_network_security_group" "nsg_db" {
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
+
+  security_rule {
+    name                        = "AllowREDIS"
+    priority                    = 1003
+    direction                   = "Inbound"
+    access                      = "Allow"
+    protocol                    = "Tcp"
+    source_port_range           = "*"
+    destination_port_range      = "6379"
+    source_address_prefix       = "*"
+    destination_address_prefix  = "*"
+  }
+
+  security_rule {
+    name                        = "AllowPostgre"
+    priority                    = 1004
+    direction                   = "Inbound"
+    access                      = "Allow"
+    protocol                    = "Tcp"
+    source_port_range           = "*"
+    destination_port_range      = "5432"
+    source_address_prefix       = "*"
+    destination_address_prefix  = "*"
+  }
 }
--- Terraform/vnet/main.tf
+++ Terraform/vnet/main.tf
@@ -19,9 +19,29 @@ resource "azurerm_subnet" "private" {
   address_prefixes     = ["10.0.2.0/24"]
 }
 
+resource "azurerm_route_table" "main" {
+  name                = "mainRouteTable"
+  location            = azurerm_virtual_network.main.location
+  resource_group_name = azurerm_virtual_network.main.resource_group_name
+}
+
+resource "azurerm_route" "public_to_private" {
+  name                   = "publicToPrivate"
+  resource_group_name    = azurerm_virtual_network.main.resource_group_name
+  route_table_name       = azurerm_route_table.main.name
+  address_prefix         = "10.0.2.0/24"
+  next_hop_type          = "VnetLocal"
+}
+
+resource "azurerm_subnet_route_table_association" "public" {
+  subnet_id      = azurerm_subnet.public.id
+  route_table_id = azurerm_route_table.main.id
+}
+
 # resource "azurerm_subnet" "app_gateway" {
 #   name                 = "app-gateway"
 #   resource_group_name  = azurerm_virtual_network.main.resource_group_name
 #   virtual_network_name = azurerm_virtual_network.main.name
 #   address_prefixes     = ["10.0.3.0/24"]
 # }
+
